37985a72852ba28fcf99f0a4a764fd3b637798da
MalcomMinissale <95878951+MalcomMinissale@users.noreply.github.com>
2023-03-18 17:26:42 -0300

    hechos los filtros dobles

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,7 @@
 
   //-----------extracción dinámica de las categorías
-
+  let sectionCards = document.querySelector(".sectionCards");
+  let eventos = data.events
   let categories = [];
 
   for (let i = 0; i < data.events.length; i++) {
@@ -11,275 +12,60 @@
 
   for (let i = 0; i < categories.length; i++) {
     let idCategories = document.getElementById("categories");
-
     let divCategory = document.createElement("div");
-
     let eventCategory = `
-
         <input type="checkbox" id="${categories[i]}" name="${categories[i]}" value="${categories[i]}">
         <label for="${categories[i]}">${categories[i]}</label>
-
-    
         `;
-
     divCategory.innerHTML += eventCategory;
-
     idCategories.appendChild(divCategory);
-
     console.log(categories.indexOf);
   }
 
+  //-------------filtros--------------
+
+  crearUnaTarjeta(eventos)
+
+  const input = document.getElementById('search')
+  let dataInput;
+  const checkboxs = document.querySelectorAll("input[type='checkbox']");
+  function filtroCards(e){
+    e.preventDefault()
+    dataInput = input.value.trim().toLowerCase();
+    let categoriasSeleccionadas = Array.from(checkboxs).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value.toLowerCase());
+    let busquedaFiltrada = data.events.filter(function (evento){
+      let filtroSearch = evento.name.toLowerCase().includes(dataInput) || evento.description.toLowerCase().includes(dataInput)
+      let filtroCategorias = categoriasSeleccionadas.length == 0 || categoriasSeleccionadas.includes(evento.category.toLowerCase())
+      console.log(filtroSearch)
+      return filtroSearch && filtroCategorias
+    })
+    if (busquedaFiltrada.length > 0){
+      crearUnaTarjeta(busquedaFiltrada)
+    } 
+    }
+  
+  
+  input.addEventListener("input", filtroCards)
+  checkboxs.forEach(checkbox => checkbox.addEventListener('change', filtroCards))
+
 //----------creación de una tarjeta---------------
 
-const checkboxs = document.querySelectorAll("input[type='checkbox']");
-
-let categoriasSeleccionadas = [];
-
-
-checkboxs.forEach(checkbox => {
-  checkbox.addEventListener("click", () => {
-    let categoriasSeleccionadas = Array.from(checkboxs).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-    crearUnaTarjeta(categoriasSeleccionadas, ".sectionCards")
-    console.log(categoriasSeleccionadas)
-  })
-})
-
-
-function crearUnaTarjeta(categoriasSeleccionadas, contenedor) {
-  let sectionCards = document.querySelector(contenedor);
-
-  sectionCards.innerHTML = "";
-
-  let eventosFiltrados = data.events.filter(evento => {
-    let categoriasEvento = evento.category;
-    if (categoriasSeleccionadas.length > 0){
-      return categoriasSeleccionadas.filter(categoria => categoriasEvento.includes(categoria)).length > 0
-    } else {
-      return categoriasSeleccionadas.filter(categoria => !categoriasEvento.includes(categoria))
-    }
-    
-  })
-
-/*   for (let i = 0; i < data.events.length; i++) { */
-/*     let sectionCards = document.querySelector(".sectionCards"); */
-
-    eventosFiltrados.forEach(evento => {
-
-    
-    let card = document.createElement("div");
-    card.classList = "card";
-
-    card.innerHTML += `
-      <img src=${evento.image} class="card-img-top">
-      <div class="card-body">
-          <h5>${evento.name}</h5>
-         
-            <div class="footer-card">
-              <p>Price: $${evento.price}</p>
-              <a href="./details.html?id=${evento._id}">Ver más</a>
-            </div>
-      </div>        
-      `;
-
-    /* console.log(card); */
-    sectionCards.appendChild(card);
-  });
-  }
-  
-/* } */
- crearUnaTarjeta(categoriasSeleccionadas, ".sectionCards");
-  /*   } */
-
-  // console.log(data.events[i].category)
-
-
-
-
-
-
-
-
-  //-------------creación de Array con checkbox seleccionados-----------
-
- 
-/*   checkboxs.forEach((unNombre) =>
-    unNombre.addEventListener("click", function reconocerCheckbox(checkbox){
-      if (checkbox.target.checked){
-        arrayChecked.push(checkbox.target.value)
-        console.log(arrayChecked)
-      } else {
-        arrayChecked = arrayChecked.filter(categoria => categoria !== checkbox.target.value)
-        console.log(arrayChecked)
-      }
-    }
-  ))
- */
-
-
-
-
- /*  for (let i = 0; i < arrayChecked.length; i++){
-    if (arrayChecked == data.events.category){
-    crearUnaTarjeta(arrayChecked, ".sectionCards")
-    } else {
-      crearUnaTarjeta(data.events, ".sectionCards")
-    }
-  }
- */
-
-/*   checkboxs2.forEach((unNombre) =>
-  unNombre.addEventListener("click", function reconocerCheckbox(checkbox){
-    if (checkbox.target.checked == data.events.category){
-      arrayChecked.push(data.events)
-      console.log(arrayChecked)
-    } else {
-      arrayChecked = arrayChecked.filter(categoria => categoria !== checkbox.target.value)
-      console.log(arrayChecked)
-    }
-  }
-))
- */
-
-
-
-
-
-
-
-
-  
-/* let filtrarPorCheckbox(){
-  if (arrayChecked.length > 0){
-    crearUnaTarjeta(arrayChecked, ".sectionCards")
-  } else {
-    crearUnaTarjeta(data.events, ".sectionCards")
-  }
-} */
-
-
-
-
-/* let checkButton = document.querySelectorAll("input[type='checkbox']")
-let eventsChecked = []
-
-checkButton.forEach(boton => boton.addEventListener('change', check))
-function check(){
-  eventsChecked = []
-    let select = Array.from(checkButton).filter(e => e.checked)
-    for (let event of data.events) {
-      select.forEach(input => {
-        if (event.category == input.value) {
-          eventsChecked.push(event)
-          console.log(eventsChecked)
-        }
-      });
-    }
-    if (eventsChecked.length > 0){
-      crearUnaTarjeta(eventsChecked, ".sectionCards")
-    } else {
-      crearUnaTarjeta(data.events, ".sectionCards")
-    }
-}
-
-
-
- */
-
-
-
-
-  
-
- /*  function reconocerCheckbox() {
-    arrayChecked = [];
-  }
-  let arrCheckbox = Array.from(checkboxs2).filter((boton) => boton.checked);
-  for (const event of data.events) {
-    arrCheckbox.forEach((input) => {
-      if (event.category == input.value) {
-        arrayChecked.push(input.value);
-      }
+function crearUnaTarjeta(arr) {
+  let mostrar = ""
+  arr.forEach(evento => {    
+    mostrar += `<div class="card">
+        <img src=${evento.image} class="card-img-top">
+        <div class="card-body">
+            <h5>${evento.name}</h5>
+              <div class="footer-card">
+                <p>Price: $${evento.price}</p>
+                <a href="./details.html?id=${evento._id}">Ver más</a>
+              </div>
+        </div>       
+        </div>         
+        `;
     });
+    sectionCards.innerHTML = mostrar
+   
   }
-  if (arrayChecked.length > 0) {
-    crearUnaTarjeta(arrayChecked, ".sectionCards");
-  } else {
-    crearUnaTarjeta(data.events, ".sectionCards");
-  }
-
-  console.log(arrayChecked); */
-
-
-/* checkboxs2.forEach(item => {
-  item.addEventListener('change', handleClick) 
-    function handleClick(e){
-      console.log(e)
-      console.log({target : e.target})
-    }
-  })
   
- */
-
-/* let arrCheckbox = Array.from(checkboxs2).filter(boton => boton.checked) */
-
-/* console.log(arrCheckbox)
-
-
-
-/* let checkboxs = document.getElementById("categories") */
-/* for(let checkbox of arrCheckbox){
-  checkbox.addEventListener("change", (item) => {
-    if (item.target.checked){
-      arrayChecked.push(item.target.value);
-    
-    }
-  })
- 
-}
-
-
-console.log(arrayChecked) 
-
- */
-/*  (checkbox) => {
-    if (checkbox.checked){
-      arrayChecked.push(checkbox.value)
-    }
-  })
-  console.log(arrayChecked)
-}
- */
-
-/* checkboxs.addEventListener('click', handleClick)
-
-function handleClick(e){
-  console.log(e)
-  console.log({target : e.target})
-}
-
-console.log(checkboxs2)
-
-
- arrayChecked = Array.from(checkboxs2).filter(checkbox => checkbox.checked){
-  arrayChecked.push(checkbox)
-
- }
- console.log(arrayChecked)
-
- */
-
-/* 
-
- let search = ""
-
- const form = document.getElementById('form')
-
- const input = document.getElementById('search')
-
-form.addEventListener('submit', (e)=> {
-  e.preventDefault()
-
-  console.log(input.value)
-  search.push(input.value)
-  console.log(search)
-}) */
--- scripts/pastEvents.js
+++ scripts/pastEvents.js
@@ -1,330 +1,80 @@
-var data = {
-    "currentDate": "2022-01-01",
-    "events": [
-      {
-        _id: 1,
-        "image":"https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
-        "name":"Collectivities Party",
-        "date":"2021-12-12",
-        "description":"Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-        "category":"Food Fair",
-        "place":"Room A",
-        "capacity":45000,
-        "assistance":42756,
-        "price":5
-      },
-      {
-        _id: 2,
-        "image":"https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
-        "name":"Korean style",
-        "date":"2022-08-12",
-        "description":"Enjoy the best Korean dishes, with international chefs and awesome events.",
-        "category":"Food Fair",
-        "place":"Room A",
-        "capacity":45000,
-        "assistance":42756,
-        "price":10
-      },
-      {
-        _id: 3,
-        "image":"https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
-        "name":"Jurassic Park",
-        "date":"2021-11-02",
-        "description":"Let's go meet the biggest dinosaurs in the paleontology museum.",
-        "category":"Museum",
-        "place":"Field",
-        "capacity":82000,
-        "assistance":65892,
-        "price":15
-      },
-      {
-        _id: 4,
-        "image":"https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
-        "name":"Parisian Museum",
-        "date":"2022-11-02",
-        "description":"A unique tour in the city of lights, get to know one of the most iconic places.",
-        "category":"Museum",
-        "place":"Paris",
-        "capacity":8200,
-        "estimate":8200,
-        "price":3500
-      },
-      {
-        _id: 5,
-        "image":"https://i.postimg.cc/KYD0jMf2/comicon.jpg",
-        "name":"Comicon",
-        "date":"2021-02-12",
-        "description":"For comic lovers, all your favourite characters gathered in one place.",
-        "category":"Costume Party",
-        "place":"Room C",
-        "capacity":120000,
-        "assistance":110000,
-        "price":54
-      },
-      {
-        _id: 6,
-        "image":"https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
-        "name":"Halloween Night",
-        "date":"2022-02-12",
-        "description":"Come with your scariest costume and win incredible prizes.",
-        "category":"Costume Party",
-        "place":"Room C",
-        "capacity":12000,
-        "estimate":9000,
-        "price":12
-      },
-      {
-        _id: 7,
-        "image":"https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
-        "name":"Metallica in concert",
-        "date":"2022-01-22",
-        "description":"The only concert of the most emblematic band in the world.",
-        "category":"Music Concert",
-        "place":"Room A"
-        ,"capacity":138000,
-        "estimate":138000,
-        "price":150
-      },
-      {
-        _id: 8,
-        "image":"https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
-        "name":"Electronic Fest",
-        "date":"2021-01-22",
-        "description":"The best national and international DJs gathered in one place.",
-        "category":"Music Concert",
-        "place":"Room A",
-        "capacity":138000,
-        "assistance":110300,
-        "price":250
-        },
-      {
-        _id: 9,
-        "image":"https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
-        "name":"10K for life",
-        "date":"2021-03-01",
-        "description":"Come and exercise, improve your health and lifestyle.",
-        "category":"Race",
-        "place":"Soccer field",
-        "capacity":30000,
-        "assistance":25698,
-        "price":3
-      },
-      {
-        _id: 10,
-        "image":"https://i.postimg.cc/zv67r65z/15kny.jpg",
-        "name":"15K NY",
-        "date":"2022-03-01",
-        "description":"We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-        "category":"Race",
-        "place":"New York",
-        "capacity":3000000,
-        "assistance":2569800,
-        "price":3
-        },
-      {
-        _id: 11,
-        "image":"https://i.postimg.cc/Sst763n6/book1.jpg",
-        "name":"School's book fair",
-        "date":"2022-10-15",
-        "description":"Bring your unused school book and take the one you need.",
-        "category":"Book Exchange",
-        "place":"Room D1",
-        "capacity":150000,
-        "estimate":123286,
-        "price":1
-      },
-      {
-        _id: 12,
-        "image":"https://i.postimg.cc/05FhxHVK/book4.jpg",
-        "name":"Just for your kitchen",
-        "date":"2021-11-09",
-        "description":"If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-        "category":"Book Exchange",
-        "place":"Room D6",
-        "capacity":130000,
-        "assistance":90000,
-        "price":100
-      },
-      {
-        _id: 13,
-        "image":"https://i.postimg.cc/vH52y81C/cinema4.jpg",
-        "name":"Batman",
-        "date":"2021-03-11",
-        "description":"Come see Batman fight crime in Gotham City.",
-        "category":"Cinema",
-        "place":"Room D1",
-        "capacity":11000,
-        "assistance":9300,
-        "price":225
-      },
-      {
-        _id: 14,
-        "image":"https://i.postimg.cc/T3C92KTN/scale.jpg",
-        "name":"Avengers",
-        "date":"2022-10-15",
-        "description":"Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-        "category":"Cinema",
-        "place":"Room D1",
-        "capacity":9000,
-        "estimate":9000,
-        "price":250
-      }
-    ]
-  };
 
-/*   let tituloCard = document.getElementsByClassName('card-title')
-  tituloCard = data.events[0].name
-  console.log(tituloCard)
- 
- 
- 
- 
-  console.log(data.events[0].price)
-
-  for(let i = 0;i < data.events.length; i++){
-    let element = data.events[i].name;
-    console.log(element)
-  }
-  
-
- */
-
-
-
-
-
-// segundo intento
-
-  /* let tituloCard = document.getElementsByClassName('card-title')
-  tituloCard = data.events[0].name
-  console.log(tituloCard)
- 
- 
- 
- 
-  console.log(data.events[0].price)
-
-  for(let i = 0;i < data.events.length; i++){
-    let tituloCard = data.events[i].name;
-    console.log(tituloCard)
-  }
- */
-/* 
-  let titulosCard = document.getElementsByClassName('card-title')
-  
-  console.log(titulosCard)
- 
- 
- 
- 
-  console.log(data.events[0].price)
-
-  for(let i = 0;i < data.events.length; i++){
-    let tituloCard = data.events[i].name;
-    console.log(tituloCard)
-  }
-
-
-
- */
-
-  for(let i = 0;i < data.events.length; i++){
-
+  //-----------extracción dinámica de las categorías
   let sectionCards = document.querySelector(".sectionCards");
-
-  let card = document.createElement('div')
-  card.classList = 'card';
-
-  let eventCard = `
-      <img src=${data.events[i].image} class="card-img-top">
-      <div class="card-body">
-          <h5>${data.events[i].name}</h5>
-         
-            <div class="footer-card">
-              <p>Price: $${data.events[i].price}</p>
-              <a href="./details.html?id=${data.events[i]._id}">Ver más</a>
-            </div>
-      </div>        
-      `;
-
-
-    if (data.events[i].date < data.currentDate){
-        
-    card.innerHTML += eventCard;
-    
-    sectionCards.appendChild(card);
-    }
-  };
-
+  let eventos = data.events
   let categories = [];
+  let pastEvents = []
+  const currentDate = new Date(data.currentDate)
 
-    for(let i = 0; i < data.events.length; i++){
-
-        
-        if(!categories.includes(data.events[i].category)){
-            categories.push(data.events[i].category)
-        }
+  data.events.forEach(event => {
+    const eventDate = new Date(event.date)
+    if (eventDate < currentDate){
+      pastEvents.push(event)
     }
+  })
 
-        for(let i = 0;i < categories.length; i++){
-
-        let idCategories = document.getElementById("categories") 
-
-        let divCategory = document.createElement("div")
-
-        let eventCategory = `
-
-        <input type="checkbox" id="category${[i]}" name="category${[i]}" value="${[i]}">
-        <label for="category${[i]}">${categories[i]}</label>
-
-             
-        `;
-
-        divCategory.innerHTML += eventCategory
-
-        idCategories.appendChild(divCategory)
-
-        console.log(categories.indexOf)
-
-        
-        }
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let dataFromCheckbox
-
-const checkbox = document.getElementById('categories')
-
-let = arrayCheckbox = Array.from(checkbox)
-
-arrayCheckbox.addEventListener("change", dataCheckbox)
-function dataCheckbox (evento){
-  let checked = []
-  let checkboxes = evento.target().querySelectorAll('input[type=checkbox]')
-
-  for (let i = 0; i < checkboxes.length; i++){
-    if (checkboxes[i].checked){
-      checked.push(checkboxes[i].value)
+  for (let i = 0; i < data.events.length; i++) {
+    if (!categories.includes(data.events[i].category)) {
+      categories.push(data.events[i].category);
     }
   }
-  return checked
-}
-console.log(dataFromCheckbox)
+
+  for (let i = 0; i < categories.length; i++) {
+    let idCategories = document.getElementById("categories");
+    let divCategory = document.createElement("div");
+    let eventCategory = `
+        <input type="checkbox" id="${categories[i]}" name="${categories[i]}" value="${categories[i]}">
+        <label for="${categories[i]}">${categories[i]}</label>
+        `;
+    divCategory.innerHTML += eventCategory;
+    idCategories.appendChild(divCategory);
+    console.log(categories.indexOf);
+  }
+
+  //-------------filtros--------------
+
+  crearUnaTarjeta(pastEvents)
+
+  const input = document.getElementById('searchPast')
+  let dataInput;
+  const checkboxs = document.querySelectorAll("input[type='checkbox']");
+  function filtroCards(e){
+    e.preventDefault()
+    dataInput = input.value.trim().toLowerCase();
+    let categoriasSeleccionadas = Array.from(checkboxs).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value.toLowerCase());
+    let busquedaFiltrada = pastEvents.filter(function (evento){
+      let filtroSearch = evento.name.toLowerCase().includes(dataInput) || evento.description.toLowerCase().includes(dataInput)
+      let filtroCategorias = categoriasSeleccionadas.length == 0 || categoriasSeleccionadas.includes(evento.category.toLowerCase())
+      console.log(filtroSearch)
+      return filtroSearch && filtroCategorias
+    })
+    if (busquedaFiltrada.length > 0){
+      crearUnaTarjeta(busquedaFiltrada)
+    } 
+    }
+  
+  
+  input.addEventListener("input", filtroCards)
+  checkboxs.forEach(checkbox => checkbox.addEventListener('change', filtroCards))
+
+//----------creación de una tarjeta---------------
+
+function crearUnaTarjeta(arr) {
+  let mostrar = ""
+  arr.forEach(evento => {    
+    mostrar += `<div class="card">
+        <img src=${evento.image} class="card-img-top">
+        <div class="card-body">
+            <h5>${evento.name}</h5>
+              <div class="footer-card">
+                <p>Price: $${evento.price}</p>
+                <a href="./details.html?id=${evento._id}">Ver más</a>
+              </div>
+        </div>       
+        </div>         
+        `;
+    });
+    sectionCards.innerHTML = mostrar
+   
+  }
+  
